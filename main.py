a753bc2a2449126d390702801e01aa8b8ca5edf2
Glebalostib <77092459+glebzalostib@users.noreply.github.com>
2022-01-20 12:56:22 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,10 @@
+texi = input("Ievadit tekstu: ")
+
+def tekstaaas():
+  if texi.count('o') or texi.count('o'):
+        a = texi.replace('o','%')
+        a = texi.replace('o','%')
+        print(a)
+  else:
+    print('nav O')
+tekstaaas()
